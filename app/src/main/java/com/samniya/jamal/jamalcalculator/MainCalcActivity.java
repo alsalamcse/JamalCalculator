a1b804f6884a106f1581f08e7185efa3de66b609
Adam Awad <adam.awad.2000@hotmail.com>
2017-10-11 12:26:38 +0300

    login

--- app/src/main/java/com/samniya/jamal/jamalcalculator/MainCalcActivity.java
+++ app/src/main/java/com/samniya/jamal/jamalcalculator/MainCalcActivity.java
@@ -16,13 +16,14 @@ public class MainCalcActivity extends AppCompatActivity implements View.OnClickL
     private Button button4;
     private Button btneq;
     private Button butoon6;
-    private Button button7;
+    private Button btnclear;
     private EditText etnumber1;
     private EditText etnumber2;
     private EditText etfinishnumber;
-    private TextView unknown;
-    private TextView result;
-}
+    private TextView tvunknown;
+    private TextView tvresult;
+
+
 
 
     @Override
@@ -36,23 +37,89 @@ public class MainCalcActivity extends AppCompatActivity implements View.OnClickL
         button4=(Button)findViewById(R.id.button4);
         btneq=(Button)findViewById(R.id.btneq);
         btneq=(Button)findViewById(R.id.button6);
-        button7=(Button)findViewById(R.id.button7);
+        btnclear=(Button)findViewById(R.id.btnClear);
         etnumber1=(EditText)findViewById(R.id.etnumber1);
         etnumber2=(EditText)findViewById(R.id.etnumber2);
         etfinishnumber=(EditText)findViewById(R.id.etfinishnumber);
-        unknown=(TextView)findViewById(R.id.unknown);
-        result=(TextView)findViewById(R.id.result);
+        tvunknown=(TextView)findViewById(R.id.unknown);
+        tvresult=(TextView)findViewById(R.id.result);
         btnminos.setOnClickListener(this);
         btnplus.setOnClickListener(this);
         button3.setOnClickListener(this);
         button4.setOnClickListener(this);
         btneq.setOnClickListener(this);
         butoon6.setOnClickListener(this);
-        button7.setOnClickListener(this);
+        btnclear.setOnClickListener(this);
     }
 
     @Override
-    public void onClick(View view) {
+    public void onClick(View view)
+    {
+      String stNum1=etnumber1.getText().toString();
+        double num1=Double.parseDouble(stNum1);
+        String stNum2=etnumber2.getText().toString();
+        double num2=Double.parseDouble(stNum2);
+        String stNum3=etfinishnumber.getText().toString();
+        double num3=Double.parseDouble(stNum3);
+        if(view==btnplus);
+        {
+            tvunknown.setText("+");
+
+        }
+        if (view==btnminos);
+        {
+            tvunknown.setText("-");
+
+        }
+        if(view==button3);
+        {
+            tvunknown.setText(":");
+
+        }
+        if(view==button4);
+        {
+            tvunknown.setText("x");
+
+        }
+        if(view==btneq);
+        {
+            String unknown=tvunknown.getText().toString();
+            double res=0;
+            switch (unknown)
+            {
+                case"+":
+                    res=num1+num2;
+                    etfinishnumber.setText(res+"");
+                    break;
+                case"-":
+                    res=num1-num2;
+                    etfinishnumber.setText(res+"");
+                    break;
+                case"x":
+                    res=num1*num2;
+                    etfinishnumber.setText(res+"");
+
+
+            }
+
+
+
+
+        }
+        if(view==butoon6);
+        {
+            tvunknown.setText("^");
+
+        }
+        if(view==btnclear);
+        {
+            etnumber1.setText("");
+            etnumber2.setText("");
+            etfinishnumber.setText("");
+            tvunknown.setText("?");
+
+        }
+
 
     }
 }
